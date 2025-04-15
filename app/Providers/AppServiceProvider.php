7b0acf25d7b0c48102391b33eb52b50e25c9a312
klometerz <kafik9@gmail.com>
2025-04-15 12:58:53 +0700

    Update AppServiceProvider.php
    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -17,10 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
-{
-    if (env('APP_ENV') === 'production') {
-        URL::forceScheme('https');
+    public function boot(): void
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
-}
 }
